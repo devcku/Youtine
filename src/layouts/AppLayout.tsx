2ad68a18e9ec00ea7d93e40ca64294145d9a8a15
Mike <chukwumichael70@gmail.com>
2024-06-14 11:07:45 +0100

    started layout

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -4,7 +4,7 @@ const AppLayout = () => {
 	return (
 		<main className="fixed h-full w-full grid grid-cols-5 bg-zinc-50">
 			<nav className="grid-cols-2 bg-zinc-100"></nav>
-			<div className="col-span-3">
+			<div className="col-span-3 p-4">
 				<Outlet />
 			</div>
 		</main>
